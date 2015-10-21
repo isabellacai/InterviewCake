2d53abf28bb144452eb1c9be82b3952260464043
Isabella Cai <isabella.cai@students.makeschool.com>
2015-10-21 13:36:43 -0700

    added code for deleting a node in a linked list in constant time

---
+++ delete-node.py
@@ -0,0 +1,10 @@
+def delete_node(node_to_delete):
+    next_node  = node_to_delete.next
+
+    if next_node:
+        node_to_delete.value = next_node.value
+        node_to_delete.next = next_node.next
+
+
+    else:
+        raise Exception("Can't delete the node!")
