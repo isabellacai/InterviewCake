384be7a8bb37f19c8a1312654ad7a9ce5c62442b
Isabella Cai <isabella.cai@students.makeschool.com>
2015-10-26 01:01:35 -0700

    still working on 'which appears twice'

--- which-appears-twice.py
+++ which-appears-twice.py
@@ -1,10 +1,14 @@
 arr = [1, 2, 3, 4, 4, 5]
 def num_appear_twice(n):
     # hashmap = {} # key: number, value: number of times that number appears
+    # for each_num in range(arr)
+    # for each_num in range (1, n+1):
+        prev_num = None
 
-    for each_num in range (1, n+1):
         for each_num in arr:
-            if each_num+1 == each_num:
+            prev_num = each_num
+            each_num =
+            if each_num == prev_num:
                 return each_num
 
 print(num_appear_twice(6))
